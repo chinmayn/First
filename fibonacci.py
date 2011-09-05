573cf0c53728bad319224502d6f1221dc7e6f312
Chinmay Nivargi <chinmay.nivargi@gmail.com>
2011-09-04 18:29:52 -0700

    fourth commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,31 @@
 #Fibonacci series
-x=input("What is the desired length of the Fibonacci sequence?")
+x=None
+while x==None:
+    try:
+        x=int(raw_input("What is the desired length of the Fibonacci sequence?"))
+        if x<1:
+            print "Please enter a positive integer"
+            x=None
+        elif x>50:
+            print "Small numbers please"
+            x=None
+    except ValueError:
+        print "Please enter a valid number"
 if x==1:
-    print "[1]"
+    print "1"
 elif x==2:
-    print "[1,1]"
+    print "1",
+    print "1",
 else:
-    sequence = [1,1]
-    a=2
-    while a<=x-1:
-        c=sequence[a-2]+sequence[a-1]
-        sequence.append(c)
-        a=a+1
-    print sequence
-    
+    a=1
+    b=1
+    print a,
+    print b,
+    i=3
+    while i<=x:
+        c=a+b
+        print c,
+        a=b
+        b=c
+        i=i+1
+
