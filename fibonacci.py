e0c5ee2543dff11017b65e5f8d00e395e8af45de
Chinmay Nivargi <chinmay.nivargi@gmail.com>
2011-09-04 19:08:57 -0700

    command line imput commit

--- fibonacci.py
+++ fibonacci.py
@@ -1,30 +1,30 @@
 #Fibonacci series
-x=None
-while x==None:
-    try:
-        x=int(raw_input("What is the desired length of the Fibonacci sequence?"))
-        if x<1:
-            print "Please enter a positive integer"
-            x=None
-        elif x>50:
-            print "Small numbers please"
-            x=None
-    except ValueError:
-        print "Please enter a valid number"
-if x==1:
-    print "1"
-elif x==2:
-    print "1",
-    print "1",
+import sys
+try:
+    #x=int(raw_input("What is the desired length of the Fibonacci sequence?"))
+    x=int(sys.argv[1])
+    if x<1:
+        print "Please enter a positive integer"
+            #x=None
+    elif x>50:
+        print "Small numbers please"            #x=None
+except ValueError:
+    print "Please enter a valid number"
 else:
-    a=1
-    b=1
-    print a,
-    print b,
-    for i in range(3,x+1,1):
-        c=a+b
-        print c,
-        a=b
-        b=c
-        i=i+1
+    if x==1:
+        print "1"
+    elif x==2:
+        print "1",
+        print "1",
+    else:
+        a=1
+        b=1
+        print a,
+        print b,
+        for i in range(3,x+1,1):
+            c=a+b
+            print c,
+            a=b
+            b=c
+            i=i+1
 
